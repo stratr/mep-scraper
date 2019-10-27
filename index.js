00193fb503c0f7cda1644ddab2264675350a1e35
Taneli <taneli.salonen@gmail.com>
2019-10-27 20:22:22 +0200

    Get twitter screen names from twitter api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
 const bucket = storage.bucket('mep-details');
+const Twitter = require('twitter');
+require('dotenv').config();
 
 const fs = require('fs');
 const fsPromises = require("fs").promises;
@@ -9,6 +11,15 @@ const cheerio = require('cheerio')
 const axios = require("axios");
 
 const siteUrl = "https://www.eduskunta.fi/FI/kansanedustajat/Sivut/Kansanedustajat-aakkosjarjestyksessa.aspx";
+
+const keListId = 203337069; // id of the list that has all the screen names of the meps
+
+const client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
 
 const fetchData = async () => {
     const result = await axios.get(siteUrl);
@@ -25,12 +36,13 @@ const getMeps = async () => {
         linkItems.each((i, elem) => {
             const name = $(elem).find('a').text();
             const party = $(elem).find('div.description').text();
-            mepsFetched[i] = { name: name, party: party };
+            mepsFetched[i] = { name: name, party: party, screen_name: null };
         });
+        //const updatedMeps = mepsFetched;
+        //const newMeps = mepsFetched;
 
         // download the previously collected data from Storage
-        const oldMepsFile = await downloadFile(bucket.file('meps.json'));
-
+        const oldMepsFile = await downloadFile(bucket.file('meps.json')); // how to handle if he file doesn't exist?
         fs.writeFile('meps_old.json', oldMepsFile, (err) => {
             if (err) throw err;
             console.log('meps_old.json file created');
@@ -52,8 +64,22 @@ const getMeps = async () => {
         // add new meps to the json
         const updatedMeps = mepsOldJson.concat(newMeps);
 
+        /*
+        TODO here: fetch twitter usernames from the twitter list and add to the json
+        */
+
+        // Get all the Twitter user names to fetch
+        const listMembers = await fetchMembers({ list_id: keListId, count: 300 });
+        const memberScreenNames = listMembers.users.map(member => { return { screen_name: member.screen_name, name: member.name } });
+        console.log(memberScreenNames[0]);pokpok
+
+                /*
+        TODO here: add the screen names to the meps json
+        */
+
+        // upload updated json to Storage
         if (updatedMeps.length >= 200) {
-            if (newMeps.length) {
+            if (newMeps.length > 0) {
                 // write to json file
                 fs.writeFile('meps.json', JSON.stringify(updatedMeps, null, 2), (err) => {
                     if (err) throw err;
@@ -89,6 +115,10 @@ const includesMep = (mep, mepArray) => {
     });
 
     return byParty.length > 0;
+}
+
+const fetchMembers = async (params) => {
+    return client.get('lists/members', params);
 }
 
 getMeps();
